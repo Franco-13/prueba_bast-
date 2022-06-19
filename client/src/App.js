2ef2b3f83e6dedaa803ef8e2996abfe3d8874b48
Franco <fegll87@gmail.com>
2022-06-19 17:38:02 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
-import { tokenStatus } from "./redux/reducer";
+import { setUserInfo, tokenStatus } from "./redux/reducer/userSlice";
 
 import Loading from "./components/Loading";
 import AnimalRegistry from "./pages/AnimalRegistry";
@@ -16,8 +16,7 @@ import "./App.css";
 
 function App() {
   const loading = useSelector((state) => state.animals.loading);
-  const infoMessage = useSelector((state) => state.animals.createMessage);
-  const userInfo = useSelector((state) => state.animals.userInfo);
+  const infoMessage = useSelector((state) => state.animals.messageInfo);
 
   const [show, setShow] = useState(null);
 
@@ -25,30 +24,49 @@ function App() {
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
-      if (currentUser && userInfo) {
+      if (currentUser) {
         auth.currentUser?.getIdToken().then((res) => {
           dispatch(tokenStatus(res));
         });
-        setShow(
-          <Routes>
-            <Route
-              path="/animalsRegister"
-              element={
-                <>
-                  {loading && <Loading />}
-                  {infoMessage?.length > 0 && (
-                    <ModalMessageInfo infoMessage={infoMessage} />
-                  )}
-                  <AnimalRegistry />
-                </>
-              }
-            />
-            <Route
-              path="*"
-              element={<Navigate to="/animalsRegister" replace />}
-            />
-          </Routes>
-        );
+        fetch(`${process.env.REACT_APP_API}/user/auth`, {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: currentUser.uid,
+          },
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            if (data.error) {
+              throw new Error(data.error);
+            }
+            dispatch(setUserInfo(data));
+          })
+          .then(() => {
+            setShow(
+              <Routes>
+                <Route
+                  path="/animalsRegister"
+                  element={
+                    <>
+                      {loading && <Loading />}
+                      {infoMessage?.length > 0 && (
+                        <ModalMessageInfo infoMessage={infoMessage} />
+                      )}
+                      <AnimalRegistry />
+                    </>
+                  }
+                />
+                <Route
+                  path="*"
+                  element={<Navigate to="/animalsRegister" replace />}
+                />
+              </Routes>
+            );
+          })
+          .catch((err) => {
+            console.error("APP ERR", err);
+          });
       } else {
         setShow(
           <Routes>
@@ -59,7 +77,7 @@ function App() {
         );
       }
     });
-  }, [loading, infoMessage, dispatch, userInfo]);
+  }, [loading, infoMessage, dispatch]);
 
   return (
     <>
