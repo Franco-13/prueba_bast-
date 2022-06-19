888388131254e5418b8782d7e030a085b3dea8c2
Franco <fegll87@gmail.com>
2022-06-19 17:34:43 -0300

    add errors messages and styles fixes

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -2,46 +2,69 @@ import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
+import { validateRegister } from "../../helpers/validateRegister";
 
 import Button from "../../components/Button";
 
 import styles from "./register.module.css";
 
 function Register() {
-  const [input, setInput] = useState({ email: "", password: "" });
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    repeat_password: "",
+  });
+  const [errors, setErrors] = useState({});
+  const [firebaseErrors, setFirebaseErrors] = useState("");
 
   const navigate = useNavigate();
 
   const handleChange = ({ target: { name, value } }) => {
     setInput({ ...input, [name]: value });
+    setErrors(validateRegister({ ...input, [name]: value }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const { email, password } = input;
+    const resultValidation = validateRegister(input);
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((resp) => {
-        const payload = {
-          email,
-          uid: resp.user.uid,
-        };
-        fetch("http://localhost:3001/user", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
+    if (Object.keys(resultValidation).length === 0) {
+      const { email, password } = input;
+
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((resp) => {
+          const payload = {
+            email,
+            uid: resp.user.uid,
+          };
+          fetch(`${process.env.REACT_APP_API}/user`, {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log("regist data", data);
+            });
+          navigate("/");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+
+          if (errorCode === "auth/email-already-in-use") {
+            setFirebaseErrors("El correo ingresado ya se encuentra registrado");
+            setTimeout(() => {
+              setFirebaseErrors("");
+            }, 1500);
+          }
         });
-        navigate("/");
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-      });
+    } else {
+      setErrors(resultValidation);
+    }
   };
 
   return (
@@ -61,6 +84,11 @@ function Register() {
               onChange={handleChange}
               value={input.email}
             />
+            {errors?.email ? (
+              <span className={styles.span_error}>{errors?.email}</span>
+            ) : (
+              <span className={styles.empty_span}>""</span>
+            )}
           </div>
           <div className={styles.input_container}>
             <label className={styles.label} htmlFor="password">
@@ -74,7 +102,33 @@ function Register() {
               onChange={handleChange}
               value={input.password}
             />
+            {errors?.password ? (
+              <span className={styles.span_error}>{errors?.password}</span>
+            ) : (
+              <span className={styles.empty_span}>""</span>
+            )}
           </div>
+          <div className={styles.input_container}>
+            <label className={styles.label} htmlFor="password">
+              <p className={styles.p}>Confirmar password</p>
+            </label>
+            <input
+              className={styles.input}
+              placeholder="******"
+              type="password"
+              name="repeat_password"
+              onChange={handleChange}
+              value={input.repeat_password}
+            />
+            {errors?.repeat_password ? (
+              <span className={styles.span_error}>
+                {errors?.repeat_password}
+              </span>
+            ) : (
+              <span className={styles.empty_span}>""</span>
+            )}
+          </div>
+          <span className={styles.span_firebase}>{firebaseErrors}</span>
           <Button>Registrarse</Button>
         </form>
         <div>
