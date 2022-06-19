0f6ff2439d62752a3997440ebc52a8fa1642d9d4
Franco <fegll87@gmail.com>
2022-06-19 17:29:36 -0300

    add user reducer

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import animals from "../reducer";
+import users from "../reducer/userSlice";
+
+const reducer = {
+  animals,
+  users,
+};
 
 export default configureStore({
-  reducer: {
+  reducer,
+  /*   reducer: {
     animals,
-  },
+  }, */
 });
