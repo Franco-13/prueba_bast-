588826f9660c4c2faa9d37eaef753fc0a531932b
Franco <fegll87@gmail.com>
2022-06-20 13:27:15 -0300

    refactor firebase admin credentials

--- api/src/app.js
+++ api/src/app.js
@@ -5,12 +5,41 @@ import usersRoutes from "./routes/users.routes.js";
 import cors from "cors";
 import admin from "firebase-admin";
 import { readFile } from "fs/promises";
+import { config } from "dotenv";
 
-const json = JSON.parse(
+config();
+
+const {
+  FIREBASE_CLIENT,
+  FIREBASE_CLIENT_ID,
+  FIREBASE_AUTH_PROVIDER,
+  FIREBASE_TOKEN_URI,
+  FIREBASE_AUTH_URI,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_PRIVATE_KEY_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_TYPE,
+} = process.env;
+
+/* const json = JSON.parse(
   await readFile(new URL("./serviceAccountKey.json", import.meta.url))
-);
+); */
 export const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert(json),
+  credential: admin.credential.cert(
+    /* json */ {
+      type: FIREBASE_TYPE,
+      project_id: FIREBASE_PROJECT_ID,
+      private_key_id: FIREBASE_PRIVATE_KEY_ID,
+      private_key: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: FIREBASE_CLIENT_EMAIL,
+      client_id: FIREBASE_CLIENT_ID,
+      auth_uri: FIREBASE_AUTH_URI,
+      token_uri: FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER,
+      client_x509_cert_url: FIREBASE_CLIENT,
+    }
+  ),
 });
 
 const server = express();
