7c45e5294a1687679f37d952e5433929959fd35e
Franco <fegll87@gmail.com>
2022-06-20 20:16:26 -0300

    refactor model Animal

--- api/src/models/Animal.js
+++ api/src/models/Animal.js
@@ -4,12 +4,39 @@ import { ANIMAL_TYPES, DEVICE_TYPES } from "../helpers/constants.js";
 const animalSchema = new mongoose.Schema(
   {
     _id: { type: String },
-    id_senasa: { type: String, required: true, maxlength: 16, minlength: 16 },
-    animal_type: { type: String, enum: ANIMAL_TYPES },
-    animal_weight: { type: Number, required: true },
-    paddock_name: { type: String, maxlength: 200 },
-    device_type: { type: String, enum: DEVICE_TYPES },
-    device_number: { type: String, required: true, maxlength: 8, minlength: 8 },
+    id_senasa: {
+      type: String,
+      required: true,
+      maxlength: 16,
+      minlength: 16,
+    },
+    animal_type: {
+      type: String,
+      enum: ANIMAL_TYPES,
+      required: true,
+    },
+    animal_weight: {
+      type: Number,
+      required: true,
+    },
+    paddock_name: {
+      type: String,
+      maxlength: 200,
+      required: true,
+    },
+    device_type: {
+      type: String,
+      enum: DEVICE_TYPES,
+      required: true,
+    },
+    device_number: {
+      type: String,
+      required: true,
+      maxlength: 8,
+      minlength: 8,
+      unique: true,
+      index: true,
+    },
   },
   { _id: false }
 );
